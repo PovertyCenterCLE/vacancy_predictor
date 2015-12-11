d07036ec4d43fce4c4f6e2cda3500b0e5b7fa81d
cwang912 <cwang912@gmail.com>
2015-12-11 14:24:28 -0500

    refactor code so all 2. scripts can be run independently and added to 3.0

--- scripts/parse_main.py
+++ scripts/parse_main.py
@@ -7,12 +7,12 @@ path = '/'.join(os.getcwd().split('/')[:-1])
 
 # get survey data
 infile = path+'/data/original_data/main_prop.csv'
-outfile = path+'/data/clean_data/main_prop3.csv'
+outfile = path+'/data/clean_data/main_prop14.csv'
 
 with open(infile, 'r') as fin, open(outfile, 'w') as fout:
     write_to = csv.writer(fout, lineterminator='\n')
     header = next(csv.reader(fin))
     write_to.writerow(header)
     for row in csv.reader(fin):
-        if row[1] == "2013":
+        if row[1] == "2014":
             write_to.writerow(row)
